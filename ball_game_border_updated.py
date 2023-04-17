54a3ce4ad643d6bf1bed086061d10541c3c89f48
HARISREE SURESH <getharisree@outlook.com>
2023-04-17 16:17:35 +0530

    Update ball_game_border_updated.py

--- ball_game_border_updated.py
+++ ball_game_border_updated.py
@@ -4,13 +4,22 @@ import math
 
 BALL_SPEED = 10
 BALL_SPEED_INCREMENT = 1
-SCORES_PER_LEVEL = 1
+SCORES_PER_LEVEL = 2
+
+ball_color = pygame.Color('red')
+ball_speed = [5, 5]
+ball_radius = 20
 
 # declaring screen
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 600
 BALL_RADIUS = 20
 PLAYER_SPEED = 5
+
+###########
+border_width = 10
+border_color = pygame.Color('white')
+border_rect = pygame.Rect(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT)
 
 # declaring colours in game
 
@@ -32,16 +41,16 @@ class Player:
         self.held_ball = None
 
     def move_left(self):
-        self.x -= PLAYER_SPEED * 2
+        self.x -= PLAYER_SPEED * 3
 
     def move_right(self):
-        self.x += PLAYER_SPEED * 2
+        self.x += PLAYER_SPEED * 3
 
     def move_up(self):
-        self.y -= PLAYER_SPEED * 2
+        self.y -= PLAYER_SPEED * 3
 
     def move_down(self):
-        self.y += PLAYER_SPEED * 2
+        self.y += PLAYER_SPEED * 3
 
     def hold_ball(self, ball):
         self.held_ball = ball
@@ -105,12 +114,10 @@ players = [Player(SCREEN_WIDTH / 4, SCREEN_HEIGHT / 2, BLUE), Player(3 * SCREEN_
 ball = Ball(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2, 20, GREEN, 2)
 
 
-def increase_level():
+def increase_level(t):
     ball.level += 1
-    ball_speed = BALL_SPEED + ball.level * BALL_SPEED_INCREMENT
-    ball.set_speed(ball_speed, ball_speed)
-
-
+    t += 5
+    return t
 
 
 while True:
@@ -147,28 +154,55 @@ while True:
                 else:
                     ball.held_by.release_ball()
 
-        for player in players:
-            distance = math.sqrt((player.x - ball.x) ** 2 + (player.y - ball.y) ** 2)
-            if distance <= BALL_RADIUS:
-                player.score += 1
-                ball.x = random.randint(BALL_RADIUS, SCREEN_WIDTH - BALL_RADIUS)
-                ball.y = random.randint(BALL_RADIUS, SCREEN_HEIGHT - BALL_RADIUS)
-                ball.held_by = None
-    if player.score % SCORES_PER_LEVEL == 0:
-        increase_level()
+        # Check for collisions between players and ball
+    for player in players:
+        distance = math.sqrt((player.x - ball.x) ** 2 + (player.y - ball.y) ** 2)
+        if distance <= BALL_RADIUS:
+            player.score += 1
+            ball.x = random.randint(BALL_RADIUS, SCREEN_WIDTH - BALL_RADIUS)
+            ball.y = random.randint(BALL_RADIUS, SCREEN_HEIGHT - BALL_RADIUS)
+            ball.held_by = None
+        if distance > 800:
+            ball.x = random.randint(BALL_RADIUS, SCREEN_WIDTH - BALL_RADIUS)
+            ball.y = random.randint(BALL_RADIUS, SCREEN_HEIGHT - BALL_RADIUS)
+            ball.held_by = None
+
+
+    if not ball.is_held():
+        ball.move(ball.speed_x, ball.speed_y)
+
+        # Check for  walls
+        if ball.x < BALL_RADIUS or ball.x > SCREEN_WIDTH - BALL_RADIUS:
+            ball.speed_x *= -1
+        if ball.y < BALL_RADIUS or ball.y > SCREEN_HEIGHT - BALL_RADIUS:
+            ball.speed_y *= -1
+        if ball.x < BALL_RADIUS:
+            ball.x = BALL_RADIUS
+        if ball.x > SCREEN_WIDTH - BALL_RADIUS:
+            ball.x = SCREEN_WIDTH - BALL_RADIUS
+        if ball.y < BALL_RADIUS:
+            ball.y = BALL_RADIUS
+        if ball.y > SCREEN_HEIGHT - BALL_RADIUS:
+            ball.y = SCREEN_HEIGHT - BALL_RADIUS
+
+
 
     screen.fill(BLACK)
+
     BORDER_WIDTH = 10
-    border_rects = [
-        pygame.Rect(50, 0, SCREEN_WIDTH, BORDER_WIDTH),  # Top
-        pygame.Rect(0, SCREEN_HEIGHT - BORDER_WIDTH, SCREEN_WIDTH, BORDER_WIDTH),  # Bottom
+    borderrects = [
+        pygame.Rect(50, 0, SCREEN_WIDTH, BORDER_WIDTH),  # Top bordr
+        pygame.Rect(0, SCREEN_HEIGHT - BORDER_WIDTH, SCREEN_WIDTH, BORDER_WIDTH),  # Bottom bordr
         pygame.Rect(0, 0, BORDER_WIDTH, SCREEN_HEIGHT),  # Left
-        pygame.Rect(SCREEN_WIDTH - BORDER_WIDTH, 0, BORDER_WIDTH, SCREEN_HEIGHT),  # Right
+        pygame.Rect(SCREEN_WIDTH - BORDER_WIDTH, 0, BORDER_WIDTH, SCREEN_HEIGHT),  # Right wal
     ]
     border_color = pygame.Color('blue')
+    ball_pos = [SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2]
 
-    for rect in border_rects:
+    for rect in borderrects:
         pygame.draw.rect(screen, border_color, rect)
+    ball_pos[0] += ball_speed[0]
+    ball_pos[1] += ball_speed[1]
 
     # Move ball if it's not held
     if not ball.is_held():
@@ -188,12 +222,12 @@ while True:
             ball.y = random.randint(BALL_RADIUS, SCREEN_HEIGHT - BALL_RADIUS)
             ball.held_by = None
 
+
+
     # Draw players and ball
     for player in players:
         player.draw(screen)
     ball.draw(screen)
-    if player.score % SCORES_PER_LEVEL == 0:
-        increase_level()
 
     # Draw scores
     font = pygame.font.SysFont(None, 48)
@@ -201,5 +235,4 @@ while True:
     screen.blit(score_text, ((SCREEN_WIDTH - score_text.get_width()) / 2, 10))
 
     pygame.display.update()
-    clock.tick(30)
-
+    clock.tick(60)
